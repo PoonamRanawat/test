72c5c90afe6c44f2d0686147fab63eeca975ad8f
Swapnil Rawat <swapnilr@ansh.com>
2017-07-17 15:57:18 +0530

    Fixed bugs 0021922, 0021921

--- src/app/project/questionnaire-tab/questionnaire/page-list-slider/page-list/page-list.component.ts
+++ src/app/project/questionnaire-tab/questionnaire/page-list-slider/page-list/page-list.component.ts
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class PageListComponent implements OnInit {
     pageListCollapse = true;
     // todo: page list items will come from an API call
-    pageList = [{pageId: 1, pageName: 'single'}, {pageId: 2, pageName: 'double'}, {pageId: 3, pageName: 'multi'}];
+    pageList = [
+        { pageId: 1, pageName: 'page1' },
+        { pageId: 2, pageName: 'page2' },
+        { pageId: 3, pageName: 'page3' },
+        { pageId: 4, pageName: 'page4' }
+    ];
 
     constructor() {
     }
