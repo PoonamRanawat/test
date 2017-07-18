799e9dea8645edf11b3ff7e7dcd6041ff8556fbd
Swapnil Rawat <swapnilr@ansh.com>
2017-07-18 11:30:33 +0530

    Active Menu fixed

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -8,6 +8,8 @@ import {MenuService} from '../../core/menu.service';
 
 export class SideBarComponent implements OnInit {
     sidebarToggleFlag: boolean;
+    isActive: number;
+    isOpen = false;
     opened: Array<boolean> = [];
     menuItems: any;
 
@@ -17,5 +19,11 @@ export class SideBarComponent implements OnInit {
     ngOnInit() {
         this.menuItems = this.menuService.getMenuConfig('left');
         this.sidebarToggleFlag = this.menuService.getToggleClass();
+    }
+
+    activateMenuItem(i: number) {
+        this.isActive = i;
+        this.isOpen = !this.isOpen;
+        console.log(this.isActive);
     }
 }
